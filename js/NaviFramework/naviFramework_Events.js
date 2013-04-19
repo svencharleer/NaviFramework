247b05b07108e1d1bd5621373052260636f87ad0
Sven Charleer <sven.charleer@gmail.com>
2013-04-19 13:46:45 +0200

    Fix for test

--- js/NaviFramework/naviFramework_Events.js
+++ js/NaviFramework/naviFramework_Events.js
@@ -3,21 +3,21 @@ var genericTouchEvents =
 {
 	 mouseDownEvent: function(event, obj)
 				    {
-    					obj.position = event.point;
+    					//obj.position = event.point;
     				},
    	dragging_mouseDown: function(event, obj)
    	{
-   		obj.position = event.position;
+   		//obj.position = event.position;
    	},
 
    	dragging_mouseDrag: function(event, obj)
    	{
-   		obj.position = obj.position + event.delta;
+   		//obj.position = obj.position + event.delta;
    	},
 
    	dragging_mouseUp: function(event, obj)
    	{
-   		obj.position = event.position;
+   		//obj.position = event.position;
    	},
      fingerEvent: function(point, obj)
             {
